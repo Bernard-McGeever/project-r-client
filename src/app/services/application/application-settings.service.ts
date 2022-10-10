9af4336ea5f043b0dd2c3bfaa7955cda72be9c14
Bernard McGeever <78788406+Bernard-McGeever@users.noreply.github.com>
2022-10-10 12:13:47 +0100

    create application settings service to store theme in local storage (#3)

---
+++ src/app/services/application/application-settings.service.ts
@@ -0,0 +1,64 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Subscription} from "rxjs";
+import {ThemeType} from "./models/theme-type.enum";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicationSettingsService {
+
+  private themeSubject = new BehaviorSubject<ThemeType>(ThemeType.LIGHT);
+
+  constructor() {
+    this.subscribeToSettingsChanges();
+  }
+
+  private subscribeToSettingsChanges() {
+    window.addEventListener('storage', (event: StorageEvent) => {
+      if (event.key === 'theme') {
+        this.setTheme();
+      }
+    })
+  }
+
+  private setTheme() {
+    const themeString = localStorage.getItem('theme');
+    // @ts-ignore
+    const theme = themeString ? ThemeType[themeString] : ThemeType.LIGHT;
+
+    if (!this.themeSubject || this.themeSubject !== theme) {
+      this.themeSubject.next(theme);
+    }
+  }
+
+  public subscribeToTheme(next: () => void): Subscription {
+    return this.themeSubject.subscribe(next);
+  }
+
+  public get currentTheme(): ThemeType {
+    const themeString = localStorage.getItem('theme');
+    // @ts-ignore
+    const theme = themeString ? ThemeType[localStorage.getItem('theme')] : ThemeType.LIGHT;
+
+    if (!this.themeSubject.value || this.themeSubject.value !== theme) {
+      this.themeSubject.next(theme);
+    }
+
+    if (!themeString) {
+      localStorage.setItem('theme', theme);
+    }
+
+    if (!this.themeSubject || this.themeSubject !== theme) {
+      this.themeSubject.next(theme);
+    }
+
+    return theme;
+  }
+
+  public set currentTheme(theme: ThemeType) {
+    if (this.themeSubject.value !== theme) {
+      this.themeSubject.next(theme);
+    }
+    localStorage.setItem('theme', theme);
+  }
+}
